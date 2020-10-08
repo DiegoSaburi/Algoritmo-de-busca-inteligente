8f47d08dc6e8124952274b7d5430a9a9b1c6c3b4
DiegoSaburi <diego_saburi@hotmail.com>
2020-10-08 15:51:35 -0300

    Adicionada classe de pilha

---
+++ Pilha.py
@@ -0,0 +1,30 @@
+class Pilha:
+    def __init__(self, tamanho):
+        self.tamanho = tamanho
+        self.cidades = [None]*self.tamanho
+        self.topo = -1
+    
+    def empilhar(self, item):
+        if not self.pilhaCheia():
+            self.topo += 1
+            self.cidades[self.topo] = item
+        else:
+            print("Pilha Cheia")
+            
+    def desempilhar(self):
+        if not self.pilhaVazia():
+            aux = self.cidades[self.topo]
+            self.topo -= 1
+            return aux
+        else:
+            print("Pilha Vazia")
+            return None
+    
+    def getTopo(self):
+        return self.cidades[self.topo]
+    
+    def pilhaVazia(self):
+        return (self.topo == -1)
+    
+    def pilhaCheia(self):
+        return (self.topo == self.tamanho -1)
