60bdccca77dde852f4c52649da29fdb607ab7ca1
DiegoSaburi <diego_saburi@hotmail.com>
2020-10-08 13:28:32 -0300

    Adicionada classe 'cidade'

---
+++ cidade.py
@@ -0,0 +1,13 @@
+class Cidade:
+    def __init__(self, nome):
+        self.nome = nome
+        self.visitado = False
+        self.adjacentes = []
+    
+    def addCidadeAdjacente(self, cidade):
+        self.adjacentes.append(cidade)
+'''
+c = Cidade("teste")
+c.visitado
+c.
+'''
